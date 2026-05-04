7bcb81ed628f0614b53469bb86a599c48678130d
adishahirushan <adishahirushan@gmail.com>
2026-05-04 14:23:54 +0530

    Initial project structure

--- main.java
+++ main.java
@@ -8,4 +8,9 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Student Record Manager");
     }
+
+    static void addStudent(String name) {
+        students.add(name);
+        System.out.println(name + " added.");
+    }
 }
