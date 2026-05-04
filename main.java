958c27f7663a79c3b1922ad20b0f74dd1608eb19
adishahirushan <adishahirushan@gmail.com>
2026-05-04 14:20:13 +0530

    Initial project structure

---
+++ main.java
@@ -0,0 +1,11 @@
+
+import java.util.ArrayList;
+
+public class main {
+
+    static ArrayList<String> students = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Student Record Manager");
+    }
+}
